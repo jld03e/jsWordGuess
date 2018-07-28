602ff35828567b6a66c70b9bc1cccabd54a11a64
Jose De Las Salas <SueBuSo@Joses-MBP.attlocal.net>
2018-07-27 22:41:00 -0400

    added few lines of code that go somewhat according to what the instructions say we need to add, will review with TA tomorrow.

---
+++ letter.js
@@ -0,0 +1,16 @@
+function Letter(wordDisplay, guess) {
+    this.wordDisplay = wordDisplay;
+    this.guess = guess;
+    this.guessCheck = function(){
+        if (guess === i) {
+            return true
+            console.log(guess);
+        } {
+            return false
+            console.log("_");
+        }
+    };
+    this.charArg = function() {
+        
+    }
+}
